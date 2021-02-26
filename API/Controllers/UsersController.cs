f3bb123b10c53c6a0bad5c5c0e399cbcaabc65f9
Vig1lante <apakieser@gmail.com>
2021-02-26 13:07:20 +0100

    Add usercontroller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private DataContext _ctx;
+
+        public UsersController(DataContext context)
+        {
+            _ctx = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _ctx.Users.ToListAsync();
+        }
+
+        //api/users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _ctx.Users.FindAsync(id);
+        }
+    }
+}
